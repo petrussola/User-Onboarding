7552c9eaf30c28e1e1906756ea1beb3caee53d91
Pere <pere.solaclaver@gmail.com>
2019-09-19 16:04:11 +0100

    Create yup validation logic and messages, pass it to form component

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import UserForm from './Form';
+import * as yup from 'yup';
 
 function App() {
 
@@ -14,9 +15,16 @@ function App() {
   const addUser = () => {
 
   }
+
+  const validationSchema = yup.object().shape({
+    name: yup.string().required("I need a name"),
+    email: yup.string().email("This is not an email").required("I need an email"),
+    password: yup.string().min(7, "This password is too short")
+  });
+
   return (
     <div className="App">
-      <UserForm initialUser={initialUser} addUser={addUser} />
+      <UserForm initialUser={initialUser} addUser={addUser} validationSchema={validationSchema} />
     </div>
   );
 }
